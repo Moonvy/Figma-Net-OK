8bda1531bfd09f4b787ae7e79f7f2f2b8f2642ae
staff-moonvy <staff@me.moonvy.com>
2021-08-24 11:42:32 +0800

    1.2

--- app/script/testUrl.js
+++ app/script/testUrl.js
@@ -7,12 +7,13 @@ const { inspect } = require("util")
 const staticLookup = (ip, v) => (hostname, opts, cb) => cb(null, ip, v || 4)
 const staticDnsAgent = (scheme, ip) => new require(scheme).Agent({ lookup: staticLookup(ip) })
 
-module.exports = function testUrl(url, ip) {
+module.exports = function testUrl(url, ip, timeout = 5500) {
     return new Promise((resolve) => {
         let t0 = new Date().getTime()
         axios
             .get(url, {
                 httpsAgent: staticDnsAgent("https", ip),
+                timeout: timeout + 10,
             })
             .then((r) => {
                 let t1 = new Date().getTime()
