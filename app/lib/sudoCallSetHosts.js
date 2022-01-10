09ce296d11407c773d2a727d5e8a939a18445fc0
staff-moonvy <staff@me.moonvy.com>
2022-01-10 17:26:11 +0800

    2.1

--- app/lib/sudoCallSetHosts.js
+++ app/lib/sudoCallSetHosts.js
@@ -1,4 +1,5 @@
 var sudo = require("sudo-prompt")
+var os = require("os")
 var fs = require("fs")
 const chalk = require("chalk")
 const { resolve } = require("path")
@@ -7,9 +8,9 @@ function sudoCallSetHosts(hosts) {
     let nodePath
     let scriptPath = __dirname + "/setHosts.js"
 
-    nodePath = __dirname + "/../node"
+    nodePath = resolve(__dirname + "/../node")
     if (!fs.existsSync(nodePath)) {
-        nodePath = __dirname + "/../node.exe"
+        nodePath = resolve(__dirname + "/../node.exe")
         if (!fs.existsSync(nodePath)) {
             nodePath = "node"
         }
@@ -19,13 +20,24 @@ function sudoCallSetHosts(hosts) {
 
     console.log(chalk.yellow("\n 请求管理员权限 \n "))
 
-    nodePath = resolve(nodePath)
+    nodePath = nodePath
     scriptPath = resolve(scriptPath)
 
-    sudo.exec(`"${nodePath}" "${scriptPath}" ${cmd}`, { name: "FigmaNetOK" }, function (error, stdout, stderr) {
+    let sudoCmd = `"${nodePath}" "${scriptPath}" ${cmd}`
+    sudo.exec(sudoCmd, { name: "FigmaNetOK" }, function (error, stdout, stderr) {
+
         if (error) {
             if (error.message.indexOf("EPERM") > -1) {
-                console.error(chalk.red("\n 无法获取权限，请手动修改 Hosts 文件 \n "))
+                let isMac = os.platform() === "darwin"
+                if (isMac) {
+                    console.error(
+                        chalk.red(`\n 无法获取权限，可以尝试手动复制以下命令在「终端」粘贴后中执行：\n\n`),
+                        chalk.blue.bold(sudoCmd),
+                        "\n\n"
+                    )
+                } else {
+                    console.error(chalk.red("\n 无法获取权限，请手动修改 Hosts 文件 \n "))
+                }
             }
 
             throw error
